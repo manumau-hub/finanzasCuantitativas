9d253eeeb12c83a5d943b305fc9476f6f9af8deb
lmpizarro <lmpizarro@gmail.com>
2022-09-07 07:45:52 -0300

    add americans

--- webapp/app/main.py
+++ webapp/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from enum import Enum
 from typing import Optional
 from pydantic import BaseModel
@@ -7,6 +7,8 @@ from app.Codigo.opcion_europea_bs import opcion_europea_bs
 from app.Codigo.opcion_europea_bin import opcion_europea_bin
 from app.Codigo.opcion_europea_mc import opcion_europea_mc
 from app.Codigo.opcion_europea_fd import opcion_europea_fd
+from app.Codigo.opcion_americana_bin import opcion_americana_bin
+from app.Codigo.opcion_americana_fd import opcion_americana_fd
 
 app = FastAPI()
 
@@ -68,5 +70,35 @@ async def root(params: OptionParameters = Depends()):
                                   sigma=params.sigma,
                                   div=params.div,
                                   M=200)
+    else:
+        raise HTTPException(status_code=404, detail="Not found")
+
+
+    return Result(price=price, model=params.model)
+
+@app.get("/price/options/american", tags=['Americans'])
+async def root(params: OptionParameters = Depends()):
+
+    if params.model == Model.BIN:
+         price = opcion_americana_bin(tipo=params.type,
+                                  S=params.S,
+                                  K=params.K,
+                                  T=params.T,
+                                  r=params.r,
+                                  sigma=params.sigma,
+                                  div=params.div,
+                                  pasos=1000)
+    elif params.model == Model.FD:
+         price = opcion_americana_fd(tipo=params.type,
+                                  S=params.S,
+                                  K=params.K,
+                                  T=params.T,
+                                  r=params.r,
+                                  sigma=params.sigma,
+                                  div=params.div,
+                                  M=200)
+    else:
+        raise HTTPException(status_code=404, detail="Not Implemented")
+
 
     return Result(price=price, model=params.model)
