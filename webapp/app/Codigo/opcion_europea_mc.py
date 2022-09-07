b291bc63bc8345b4e732a03917ed669ba9384ce1
lmpizarro <lmpizarro@gmail.com>
2022-09-07 00:23:08 -0300

    opciones europeas

---
+++ webapp/app/Codigo/opcion_europea_mc.py
@@ -0,0 +1,37 @@
+import numpy as np
+import math
+
+"""
+opcion_europea_mc
+Def
+    Calculador del precio de una opcion Europea con el modelo de MonteCarlo
+Inputs
+    - tipo : string - Tipo de contrato entre ["CALL","PUT"]
+    - S : float - Spot price del activo
+    - K : float - Strike price del contrato
+    - T : float - Tiempo hasta la expiracion (en años)
+    - r : float - Tasa 'libre de riesgo' (anualizada)
+    - sigma : float - Volatilidad implicita (anualizada)
+    - div : float - Tasa de dividendos continuos (anualizada)
+    - pasos : int - Cantidad de caminos de montecarlo
+Outputs
+    - precio_MC: float - Precio del contrato
+"""
+
+def opcion_europea_mc(tipo, S, K, T, r, sigma, div, pasos):
+
+    z = np.random.normal(0,1,pasos)
+    opcion = np.zeros(pasos)
+    for i in range(0,pasos):
+        if tipo == "C":
+            payoff = max( 0 , S * math.exp((r-div - 0.5 * math.pow(sigma,2)) * T + sigma * math.sqrt(T)  * z[i]) - K)
+        elif tipo == "P":
+            payoff = max(0, K - S * math.exp((r-div - 0.5 * math.pow(sigma, 2)) * T + sigma * math.sqrt(T) * z[i]) )
+
+        opcion[i] = math.exp(-r * T) * payoff
+
+    precio_MC = np.mean(opcion)
+
+    #var= np.var(opcion)
+
+    return precio_MC
--- webapp/app/main.py
+++ webapp/app/main.py
@@ -1,8 +1,57 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from enum import Enum
+from typing import Optional
+from pydantic import BaseModel
+
+from app.Codigo.opcion_europea_bs import opcion_europea_bs
+from app.Codigo.opcion_europea_bin import opcion_europea_bin
+from app.Codigo.opcion_europea_mc import opcion_europea_mc
 
 app = FastAPI()
 
+class Model(str, Enum):
+    BS = 'BS'
+    BIN = 'BIN'
+    MC = 'MC'
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class OptionParameters(BaseModel):
+    tipo: str = 'C'
+    S: float = 100.0
+    K: float = 100.0
+    T: float = 1.0
+    r: Optional[float] = 0.01
+    sigma: Optional[float] = 0.1
+    div: Optional[float] = 0.0
+    model: Model = Model.BS
+
+@app.get("/price/options/bs")
+async def root(params: OptionParameters = Depends()):
+
+    if params.model == Model.BS:
+        price = opcion_europea_bs(tipo=params.tipo,
+                                  S=params.S,
+                                  K=params.K,
+                                  T=params.T,
+                                  r=params.r,
+                                  sigma=params.sigma,
+                                  div=params.div)
+    elif params.model == Model.BIN:
+         price = opcion_europea_bin(tipo=params.tipo,
+                                  S=params.S,
+                                  K=params.K,
+                                  T=params.T,
+                                  r=params.r,
+                                  sigma=params.sigma,
+                                  div=params.div,
+                                  pasos=1000)
+    elif params.model == Model.MC:
+         price = opcion_europea_mc(tipo=params.tipo,
+                                  S=params.S,
+                                  K=params.K,
+                                  T=params.T,
+                                  r=params.r,
+                                  sigma=params.sigma,
+                                  div=params.div,
+                                  pasos=1000000)
+
+    return {'price': price, 'modelo': params.model}
