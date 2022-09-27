89eb384d4c0ec63e9db5ae8ff5b0508009db54c2
lmpizarro <lmpizarro@gmail.com>
2022-09-26 23:22:45 -0300

    change variable names

--- Codigo/tests/test_mc.py
+++ Codigo/tests/test_mc.py
@@ -4,17 +4,17 @@ import sys
 sys.path.append('..')
 from opcion_europea_mc import opcion_europea_mc, opcion_europea_mc_fv
 
-pasos = 100000
+pasos = 1000000
 t1 = time.time()
 c1 = opcion_europea_mc('C', 100, 100, 1, 0.02, 0.3, 0.01, pasos)
-c3 = opcion_europea_mc('P', 100, 100, 1, 0.02, 0.3, 0.01, pasos)
+p1 = opcion_europea_mc('P', 100, 100, 1, 0.02, 0.3, 0.01, pasos)
 print(f"time {time.time() - t1}")
 
 t1 = time.time()
 c2 = opcion_europea_mc_fv('C', 100, 100, 1, 0.02, 0.3, 0.01, pasos)
-c4 = opcion_europea_mc_fv('P', 100, 100, 1, 0.02, 0.3, 0.01, pasos)
+p2 = opcion_europea_mc_fv('P', 100, 100, 1, 0.02, 0.3, 0.01, pasos)
 print(f"time {time.time() - t1}")
 
 
-print(c1, c2, c3, c4)
+print(c1, p1, c2, p2)
 
